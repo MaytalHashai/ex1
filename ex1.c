c63530b3cab7e029db6bca8fe4e4b52074bc3486
mytlhsy <maytal.hashai@gmail.com>
2024-11-14 22:36:57 +0200

    1

--- ex1.c
+++ ex1.c
@@ -12,15 +12,15 @@ int main() {
   // What bit
   /*Scan two integers (representing number and a position)
 Print the bit in this position. */
-  int num1, position1;
-  printf("Enter a number: ");
-  scanf("%d", &num1);
-  printf("Enter a position ");
-  scanf("%d", &position1);
-  int res = (num1 >> position1) & 1;
-  printf("The bit in position %d of number %d is: %d\n", num1, position1, res);
-  // printf("What bit:\n"); @@
-  
+  // int num1, position1;
+  // printf("Enter a number: ");
+  // scanf("%d", &num1);
+  // printf("Enter a position ");
+  // scanf("%d", &position1);
+  // int res = (num1 >> position1) & 1;
+  // printf("The bit in position %d of number %d is: %d\n", num1, position1, res);
+  // // printf("What bit:\n"); @@
+
   // Set bit
   printf("\nSet bit:\n");
   /*Scan two integers (representing number and a position)
@@ -28,12 +28,17 @@ Print the bit in this position. */
   Print the output
   Now make sure it's "off" (equal to 0)
   Print the output */
-  int num2, position2;
+  int num2, position2, num_with_1_add, num_with_0_add;
   printf("Please enter a number: ");
-  scanf("%d", &num2);
+  scanf("%d", &num2, &num_with_1_add, &num_with_0_add);
   printf("Please enter a position: ");
   scanf("%d", &position2);
-  int turn_on1 =
+  int turn_on1 = ((num_with_1_add >> position2) & 1) | 1;
+  num_with_1_add = num2 | (turn_on1 << position2);
+  int turn_off = (num_with_0_add >> position2) & 0;
+  num_with_0_add = num2 ^ (turn_off << position2);
+  printf("Number with bit %d set to 1: %d\n", position2, num_with_1_add);
+  printf("Number wuth bit %d set to 0: %d", position2, num_with_0_add);
 
   // Toggle bit
   printf("\nToggle bit:\n");
